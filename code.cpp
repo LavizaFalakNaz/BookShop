c49f47e5ab1902387b2fe0afc625bfde03cc464d
Laviza Falak Naz <lavizaniazi2001@gmail.com>
2023-08-28 15:30:20 +0500

    new commit

--- code.cpp
+++ code.cpp
@@ -666,3 +666,412 @@ void members::search_mem()
 	}
 }
 
+// ------------------
+// Class Sales
+// ------------------
+
+void sales::add()
+{
+	cout << "Enter Menber id :";
+	cin >> member_id;
+	cout << "Enter the book id : ";
+	cin  >> book_id;
+	cout << "Enter the quantity : ";
+	cin  >> qty;
+	stmt.str("");
+	stmt << "select price*" << qty << " from books where id = " << book_id << ";";
+	query = stmt.str();
+	q = query.c_str();
+	mysql_query(conn,q);
+	res_set = mysql_store_result(conn);
+	if ((row = mysql_fetch_row(res_set)) != NULL)
+	{
+		cout << "The bill amount : " << row[0] << endl << endl;
+		amount = (int) row[0];
+	}
+	else
+	{
+		cout << "Book Id invalid!!" << endl;
+		getch();
+		return;
+	}
+	stmt.str("");
+	stmt << "insert into sales(mem_id,book_id,qty,amt,sales_date) values (" << member_id << "," << book_id << "," << qty << "," << amount << ",curdate());";
+	query = stmt.str();
+	q = query.c_str();
+	mysql_query(conn,q);
+	// fetching invoice id...
+	stmt.str("");
+	stmt << "select inv_id from sales where mem_id = " << member_id << " AND book_id = " << book_id << " AND qty = " << qty << " AND sales_date = curdate();";
+	query = stmt.str();
+	q = query.c_str();
+	mysql_query(conn,q);
+	res_set = mysql_store_result(conn);
+	if((row = mysql_fetch_row(res_set)) != NULL)
+	{
+		cout << "The Invoice id for the bill is " << row[0] << endl << endl << endl;
+	}
+	else
+	{
+		cout << "The entered details maybe wrong." << endl << "Please Recheck and Enter again" << endl << endl << endl;
+	}
+}
+
+
+void sales::find_total_sales()
+{
+	query = "select sum(amt) from sales where year(sales_date) = year(curdate());";
+	q = query.c_str();
+	mysql_query(conn,q);
+	res_set = mysql_store_result(conn);
+	if((row = mysql_fetch_row(res_set)) != NULL)
+	{
+		cout << "Total sales this year : " << row[0] << endl << endl << endl << endl;
+	}
+}
+
+// ----------------
+// Functions
+// ----------------
+
+void book_menu();
+void sup_menu();
+void pur_menu();
+void emp_menu();
+void mem_menu();
+void sal_menu();
+
+// main menu
+
+void main_menu()
+{
+	int c;
+	cout << "*************************************************" << endl;
+	cout << "         BOOKSHOP MANGEMENT SYSTEM" << endl;
+	cout << "*************************************************" << endl;
+	cout << "   1. BOOKS" << endl;
+	cout << "   2. SUPPLIERS" << endl;
+	cout << "   3. PURCHASES" << endl;
+	cout << "   4. EMPLOYEES" << endl;
+	cout << "   5. MEMBERS" << endl;
+	cout << "   6. SALES" << endl;
+	cout << "   7. EXIT" << endl << endl << endl;
+	cout << "Enter Your Choice : ";
+	cin >> c;
+	switch (c)
+	{
+		case 1:
+			system("cls");
+			book_menu();
+			getch();
+			break;
+		case 2:
+			system("cls");
+			sup_menu();
+			getch();
+			break;
+		case 3:
+			system("cls");
+			pur_menu();
+			getch();
+			break;
+		case 4:
+			system("cls");
+			emp_menu();
+			getch();
+			break;
+		case 5:
+			system("cls");
+			mem_menu();
+			getch();
+			break;
+		case 6:
+			system("cls");
+			sal_menu();
+			getch();
+			break;
+		case 7:
+			exit(1);
+		default:
+			system("cls");
+			cout << "Wrong Input" << endl << endl << "Invalid input";
+			getch();
+			break;
+	}
+	return;
+}
+
+// book menu
+
+void book_menu()
+{
+	int c;
+	books b;
+	cout << "*************************************************" << endl;
+	cout << "                  BOOK MENU" << endl;
+	cout << "*************************************************" << endl;
+	cout << "   1. ADD" << endl;
+	cout << "   2. UPDATE PRICE" << endl;
+	cout << "   3. SEARCH" << endl;
+	cout << "   4. UPDATE STATUS" << endl;
+	cout << "   5. DISPLAY ALL" << endl;
+	cout << "   6. RETURN TO MAIN MENU" << endl << endl << endl;
+	cout << "Enter Your Choice : ";
+	cin >> c;
+	switch (c)
+	{
+		case 1:
+			b.add();
+			break;
+		case 2:
+			b.update_price();
+			break;
+		case 3:
+			b.search();
+			break;
+		case 4:
+			b.update();
+			break;
+		case 5:
+			b.display();
+			break;
+		case 6:
+			return;
+			break;
+		default:
+			cout << "Wrong Input" << endl << "Invalid input";
+			break;
+	}
+	return;
+}
+
+// supp menu
+
+void sup_menu()
+{
+	int c;
+	suppliers s;
+	cout << "*************************************************" << endl;
+	cout << "                SUPPLIER MENU" << endl;
+	cout << "*************************************************" << endl;
+	cout << "   1. ADD" << endl;
+	cout << "   2. REMOVE" << endl;
+	cout << "   3. SEARCH" << endl;
+	cout << "   4. RETURN TO MAIN MENU" << endl << endl << endl;
+	cout << "Enter Your Choice : ";
+	cin >> c;
+	switch (c)
+	{
+		case 1:
+			s.add_sup();
+			break;
+		case 2:
+			s.remove_supplier();
+			break;
+		case 3:
+			s.search_id();
+			break;
+		case 4:
+			return;
+		default:
+			cout << "Wrong Input" << endl << "Invalid input";
+			break;
+	}
+}
+
+// purchase menu
+
+void pur_menu()
+{
+	int c;
+	purchases p;
+	cout << "*************************************************" << endl;
+	cout << "                PURCHASES MENU" << endl;
+	cout << "*************************************************" << endl;
+	cout << "   1. New Order" << endl;
+	cout << "   2. View All" << endl;
+	cout << "   3. Cancel Order" << endl;
+	cout << "   4. Recieved Order" << endl;
+	cout << "   5. RETURN TO MAIN MENU" << endl << endl << endl;
+	cout << "Enter Your Choice : ";
+	cin >> c;
+	switch (c)
+	{
+		case 1:
+			p.new_ord();
+			break;
+		case 2:
+			p.view();
+			break;
+		case 3:
+			p.mar_cancel();
+			break;
+		case 4:
+			p.mark_reciv();
+			break;
+		case 5:
+			return;
+		default:
+			cout << "Wrong Input" << endl << "Invalid input";
+			break;
+	}
+}
+
+// emp_menu
+
+void emp_menu()
+{
+	int c;
+	employees e;
+	cout << "*************************************************" << endl;
+	cout << "                 EMPLOYEE MENU" << endl;
+	cout << "*************************************************" << endl;
+	cout << "   1. New Employee" << endl;
+	cout << "   2. Search Employee" << endl;
+	cout << "   3. Assign Manager" << endl;
+	cout << "   4. View All" << endl;
+	cout << "   5. Update Salary" << endl;
+	cout << "   6. RETURN TO MAIN MENU" << endl << endl << endl;
+	cout << "Enter Your Choice : ";
+	cin >> c;
+	switch (c)
+	{
+		case 1:
+			e.add_emp();
+			break;
+		case 2:
+			e.search_emp();
+			break;
+		case 3:
+			e.assign_mgr_stat();
+			break;
+		case 4:
+			e.display();
+			break;
+		case 5:
+			e.update_sal();
+			break;
+		case 6:
+			return;
+		default:
+			cout << "Wrong Input" << endl << "Invalid input";
+			break;
+	}
+}
+
+
+// mem menu
+
+void mem_menu()
+{
+	int c;
+	members m;
+	m.refresh();
+	cout << "*************************************************" << endl;
+	cout << "                 MEMBERS MENU" << endl;
+	cout << "*************************************************" << endl;
+	cout << "   1. New Member" << endl;
+	cout << "   2. Search Member" << endl;
+	cout << "   3. RETURN TO MAIN MENU" << endl << endl << endl;
+	cout << "Enter Your Choice : ";
+	cin >> c;
+	switch (c)
+	{
+		case 1:
+			m.add_mem();
+			break;
+		case 2:
+			m.search_mem();
+			break;
+		case 3:
+			return;
+		default:
+			cout << "Wrong Input" << endl << "Invalid input";
+			break;
+	}
+}
+
+// sal_menu
+
+void sal_menu()
+{
+	int c;
+	sales s;
+	cout << "*************************************************" << endl;
+	cout << "                 SALES MENU" << endl;
+	cout << "*************************************************" << endl;
+	cout << "   1. Add New Bill" << endl;
+	cout << "   2. Total Sales Of The Year" << endl;
+	cout << "   3. RETURN TO MAIN MENU" << endl << endl << endl;
+	cout << "Enter Your Choice : ";
+	cin >> c;
+	switch (c)
+	{
+		case 1:
+			s.add();
+			break;
+		case 2:
+			s.find_total_sales();
+			break;
+		case 3:
+			return;
+		default:
+			cout << "Wrong Input" << endl << "Invalid input";
+			break;
+	}
+}
+
+
+// Password Function
+
+void pass()
+{
+	int num = 0 ;
+	cout << "Enter password : ";
+	for(int i = 0; i < 4; i++)
+	{
+		num=num*10+(getch()-48);
+		cout<<"*";
+	}
+	if(num == PASSWORD)
+	{
+		cout << endl << endl << "Correct Password" << endl << endl;
+		cout << "Press any key...";
+		getch();
+	}
+	else
+	{
+		cout << endl << endl << "Incorrect Password" << endl << endl;
+		cout << "Press any key...";
+		getch();
+		exit(1);
+	}
+	return;
+}
+
+// +----------------------------------+
+// |		Main Function 	      |
+// +----------------------------------+
+
+int main()
+{
+	pass();
+	conn = mysql_init(0);
+	conn = mysql_real_connect(conn, HOST, USER, PASS, DATABASE, PORT, NULL, 0);
+	int choice;
+	if(conn)
+    {
+		while(1)
+		{
+			system("cls");
+			main_menu();
+		}
+    }
+	else
+	{
+	    system("cls");
+		cout << "Error While connection to database." << endl << "Contact Tech Expert." << endl;
+		getch();
+	}
+	return 0;
+}

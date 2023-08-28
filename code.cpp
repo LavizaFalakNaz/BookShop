305043b4ebd7abeb48cdf10ca6f32721f11e524e
Laviza Falak Naz <lavizaniazi2001@gmail.com>
2023-08-28 15:26:53 +0500

    new commit

---
+++ code.cpp
@@ -0,0 +1,16 @@
+// +----------------------------------+
+// |          HEADER FILES            |
+// +----------------------------------+
+
+#include <iostream>
+#include <windows.h>
+#include <mysql.h>
+#include <sstream>
+#include <conio.h>
+#define HOST "127.0.0.1"
+#define USER "root"
+#define PASS /*put the password of MySQL in here between ""*/
+#define DATABASE "Management"
+#define PORT 3306
+#define PASSWORD /*Set a Numeric Password for Your Application*/
+
